2abf4b95f24ff44874f1714150e9890f07fb9140
Pheng <phengyangster9@gmail.com>
2022-03-22 15:36:36 -0700

    add 3_2

---
+++ 3_2.py
@@ -0,0 +1,39 @@
+# grade = 90
+# condition = grade >=90
+# if condition:
+#     print("your grade is A") # execute when true
+# else: 
+#     print ("Your grade is not A") # execute when false
+
+# num1 = eval(input("Enter the first number: "))
+# num2 = eval(input("Enter the second number: "))
+# try:
+#     print("1", isinstance(num1, str))
+#     print("2", isinstance(num2, str))
+#     if isinstance(num1,str) or isinstance(num2, str):
+#         print("Data type not allowed! Please specify numeric value.")
+#     else:
+#         if num1 > num2:
+#             largerVal = num1
+#         else:
+#             largerVal = num2
+#         print("The larger value is " + str(largerVal)) 
+# except: 
+#     print("Data not allowed!")
+
+
+# if num1 > num2:
+#     largerVal = num1
+# else:
+#     largerVal = num2
+#     print("The larger value is " + str(largerVal)) 
+
+# num1 = input("Enter the first number: ")
+# num2 = input("Enter the second number: ")
+
+answer = eval(input("How many gallons does a ten-gallon hat hold?"))
+if (.5 <= answer <= 1):
+    print("Good, ", end="")
+else:
+    print("no, ", end="")
+print("it holds about 3/4 of a gallon.")
